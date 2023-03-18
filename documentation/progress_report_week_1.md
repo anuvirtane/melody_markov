fd5e8ab65676cc3ace6d21e5611d2227146f39a8
Anu Virtanen <virtanenanum@gmail.com>
2023-03-18 16:51:39 +0200

    add playing soundfile to experiment. update progress report week 1

--- documentation/progress_report_week_1.md
+++ documentation/progress_report_week_1.md
@@ -2,4 +2,4 @@
 
 I read through the course requirements and decided what kind of project I want to make, created the repo, readme and project definition. Did a little reading about python libraries that have to do with sound. Experimented with sounddevice library to record and play sound.
 
-Time spent: 3 h
+Time spent: 4 h
--- src/index.py
+++ src/index.py
@@ -1,11 +1,22 @@
-"""First sounddevice recording experiment"""
+"""First sounddevice recording and playing experiment: 
+run program, make sounds for 5 s
+and then hear the program play your own sounds"""
 
 import sounddevice as sd
+import soundfile as sf
 from scipy.io.wavfile import write
 
+
 fs = 44100  # Sample rate
-seconds = 3  # Duration of recording
+seconds = 5  # Duration of recording
 
 myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
 sd.wait()  # Wait until recording is finished
 write('my_first_recording.wav', fs, myrecording)  # Save as WAV file 
+
+#play:
+
+# Extract data and sampling rate from file
+data, fs = sf.read('my_first_recording.wav', dtype='float32')  
+sd.play(data, fs)
+status = sd.wait()  # Wait until file is done playing
