bc61a68091769f38c5973f329c9bd4e505755335
Anu Virtanen <virtanenanum@gmail.com>
2023-03-22 19:10:07 +0200

    use pylint

--- src/logic/trie.py
+++ src/logic/trie.py
@@ -1,4 +1,5 @@
-"""Trie data structure for storing melodies in ABC notation. Instead of notes, chords can be used."""
+"""Trie data structure for storing melodies in ABC notation.
+Instead of notes, chords can be used."""
 
 class TrieNode:
     """Single node, building block of trie structure.
@@ -8,7 +9,6 @@ class TrieNode:
     - how many times this particular melody has been inserted
       (if this node has is_end set to True)
     - child nodes, dict contains notes as keys, and nodes as values"""
-
     def __init__(self, note: str):
         self.note = note
         self.is_end = False
@@ -24,7 +24,7 @@ class Trie():
         """
         self.root = TrieNode("")
         self.output = []
-    
+
     def insert(self, melody: list):
         """Insert a melody into Trie.
         Start from the root node.
@@ -44,32 +44,30 @@ class Trie():
                 node = new_node
         node.is_end = True
         node.counter += 1
-        
+
     def dfs(self, node: TrieNode, melody_start: str):
-        """Depth first search through the trie
-        
-        Args:
-            - node: the node to start with
-            - prefix: the current prefix, for tracing a
-                word while traversing the trie
+        """Depth first search through the trie.
+        node: starting node.
+        melody_start: start of melody to look for
         """
         if node.is_end:
-            self.output.append((melody_start + node.note, node.counter))    
+            self.output.append((melody_start + node.note, node.counter))
         for child in node.children.values():
             self.dfs(child, melody_start + node.note)
-        
-    def query(self, x: str) -> list:
-        """Use input string (one or multiple notes that melody starts with) 
+
+    def query(self, melody_start: str) -> list:
+        """Use input string (one or multiple notes that melody starts with)
         to retrieve all melodies stored in
-        the trie with that starting sequence sorted by the number of 
+        the trie with that starting sequence sorted by the number of
         times they have been inserted. Return them in list.
         """
         self.output.clear()
         node = self.root
-        for note in x:
+        for note in melody_start:
             if note in node.children:
                 node = node.children[note]
             else:
                 return []
-        self.dfs(node, x[:-1])
+        self.dfs(node, melody_start[:-1])
         return sorted(self.output, key=lambda x: x[1], reverse=True)
+    
