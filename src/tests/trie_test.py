882b992d252d51befcaa2364da070bedfb021924
Anu Virtanen <virtanenanum@gmail.com>
2023-03-22 19:51:18 +0200

    add two more trie tests

--- src/tests/trie_test.py
+++ src/tests/trie_test.py
@@ -7,3 +7,13 @@ class TestTrie(unittest.TestCase):
 
     def test_trie_root_has_empty_note(self):
         self.assertEqual(self.t.root.note, "")
+
+    def test_trie_finds_inserted_sequence_with_right_start(self):
+        self.t.insert("ABC")
+        result = self.t.query("A")
+        self.assertEqual(result, [('ABC', 1)])
+    
+    def test_trie_does_not_find_inserted_sequence_with_wrong_start(self):
+        self.t.insert("ABC")
+        result = self.t.query("B")
+        self.assertEqual(result, [])
