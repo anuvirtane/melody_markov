d66a08b73f0837c7b600615f24296a0736461783
Anu Virtanen <virtanenanum@gmail.com>
2023-03-22 19:36:51 +0200

    make test class work like a class

--- src/tests/trie_test.py
+++ src/tests/trie_test.py
@@ -3,8 +3,7 @@ from logic import trie_logic
 
 class TestTrie(unittest.TestCase):
     def setUp(self):
-        print("Set up goes here")
+        self.t = trie_logic.Trie()
 
     def test_trie_root_has_empty_note(self):
-        t = trie_logic.Trie()
-        self.assertEqual(t.root.note, "")
+        self.assertEqual(self.t.root.note, "")
