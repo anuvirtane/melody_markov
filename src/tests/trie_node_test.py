fb3f533dc3ca932d6d155e5c4a8113901a41c1a0
Anu Virtanen <virtanenanum@gmail.com>
2023-03-23 19:57:42 +0200

    remove trie node test file

--- src/tests/trie_node_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from logic import trie_logic
-
-class TestTrieNode(unittest.TestCase):
-    def setUp(self):
-        self.tn = trie_logic.TrieNode("F")
-
-    def test_node_has_note(self):
-        self.assertEqual(self.tn.note, "F")
