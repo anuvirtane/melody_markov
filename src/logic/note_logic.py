3f798a77bb073fa28187b22cfa166c1dbe847765
Anu Virtanen <virtanenanum@gmail.com>
2023-03-30 20:27:13 +0300

    add first version of parsing abc notes and inserting into trie

--- src/logic/note_logic.py
+++ src/logic/note_logic.py
@@ -1,6 +1,17 @@
+"""Parse abc notes and insert into Trie. Query trie. Hard-coded values for testing purposes."""
 
 from sjkabc import parse_dir
+import trie_logic
 
+d_trie = trie_logic.Trie()
 for tune in parse_dir('../abc-notes/'):
-   print('Parsed {} with abc {} and key {}.'.format(tune.title, tune.expanded_abc, tune.key))
- 
+  # print('Parsed {} with abc {} and key {}.'.format(tune.title, tune.expanded_abc, tune.key))
+    if len(tune.key) == 1:
+        if tune.key[0] == 'C':
+            abc = tune.expanded_abc.replace("wranscriptioncjackcampin2009nomirroringorrepblicationwithotpermission", "")           
+            print('Parsed {} with abc {}'.format(tune.title, abc))
+            d_trie.insert(abc)
+d_trie.query("ggg")
+print(d_trie.output)
+        
+       
