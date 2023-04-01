756eeec6faa60a3d9842014835c7ca42056b3020
Anu Virtanen <virtanenanum@gmail.com>
2023-04-01 19:56:34 +0300

    add single unit test

---
+++ src/tests/note_test.py
@@ -0,0 +1,15 @@
+import unittest
+from logic import note_logic
+
+class TestTriesInKeys(unittest.TestCase):
+    def setUp(self):
+        self.tries = note_logic.TriesByKeys()
+
+    def test_key_longer_than_one_char_not_inserted(self):
+        self.tries.insert_notes_from_dir("../../test-notes/longkey/")
+        keys = self.tries.get_trie_keys()
+        self.assertEqual(keys, set([]))
+
+
+
+    
