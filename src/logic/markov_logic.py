20328427ae13e2c302d60b86838364a6104b512f
Anu Virtanen <virtanenanum@gmail.com>
2023-04-23 13:26:12 +0300

    remove old comments

--- src/logic/markov_logic.py
+++ src/logic/markov_logic.py
@@ -6,12 +6,6 @@ try:
 except:
     from . import note_logic
 
-# # Possible sequences of events
-# transitionName = [["SS","SR","SI"],["RS","RR","RI"],["IS","IR","II"]]
-
-# # Probabilities matrix (transition matrix)
-# transitionMatrix = [[0.2,0.6,0.2],[0.1,0.6,0.3],[0.2,0.7,0.1]]
-
 class MarkovChain:
     """Attributes:
     self.tries: Tries that contain melodies by keys
