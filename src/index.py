e02c82a2822cdbbdb688195272c83be3ba3cf2d2
Anu Virtanen <virtanenanum@gmail.com>
2023-04-26 13:55:22 +0300

    remove midi2audio and fluisynth

--- src/index.py
+++ src/index.py
@@ -7,12 +7,13 @@ from random import choice
 
 path ="abc-notes"
 key = choice(['D', 'G', 'A', 'C', 'F'])
-melody_length = choice([10, 20, 50, 75, 100])
+melody_length = choice([10, 20, 50])
 mc = markov_logic.MarkovChain(path, key)
-melody = mc.generate_melody(max_length=melody_length)
-print(f"Generated and played a melody from teaching data randomly in key {key} using length {melody_length}")
+melody = mc.generate_melody('cc', max_length=melody_length)
 with open("src/music/generated.abc", "w") as abc_file:
     abc_file.write("L:1/8\n")
+    abc_file.write(f"K:{key}\n")
     abc_file.write(melody)
+print(f"Generated and played a melody from teaching data randomly in key {key} using length {melody_length}")
 play_abc
 
