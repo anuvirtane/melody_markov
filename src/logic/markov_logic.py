179adba1c5dc6b80dbff886767689d7efcbdf08c
Anu Virtanen <virtanenanum@gmail.com>
2023-06-19 20:07:47 +0300

    fix Markov chain logic and add unit test for it

--- src/logic/markov_logic.py
+++ src/logic/markov_logic.py
@@ -20,7 +20,8 @@ class MarkovChain:
             raise ValueError("Given key should be one of these: ['D', 'G', 'A', 'C', 'F']")
         self.key = key
 
-    def generate_melody(self, given_start: str="", max_length: int=20) -> str:
+    
+    def generate_melody(self, given_start: str="", max_length: int=20, prev_states=2) -> str:
         """
         Returns a melody generated using Markov chain logic.
         Parameters:
@@ -28,23 +29,21 @@ class MarkovChain:
         max_length (int): the maximum length of the melody.
         Returns: a generated melody (str).
         """
-        if not given_start:
-            predecessors_amount = max_length
-        else:
-            predecessors_amount = len(given_start)
-            chosen_start = given_start
-        following_notes = self.tries.get_following_notes_by_key(self.key, predecessors_amount)
+        following_notes = self.tries.get_following_notes_by_key(self.key, prev_states)
         if len(following_notes) == 0:
             return ""
         if not given_start:
-            chosen_start = random.choice(list(following_notes.keys()))
-        melody = str(chosen_start).split()
-        while chosen_start in following_notes and len(melody) < max_length:
-            possibilities = following_notes[chosen_start]
+            given_start = random.choice(list(following_notes.keys()))
+        melody = given_start
+        given_start = given_start[-prev_states:]
+        while given_start in following_notes and len(melody) < max_length:
+            possibilities = following_notes[given_start]
             next_note = 'z'
             if possibilities:
                 notes_as_weighted_possibilities_list = [key for key, value in
                                           possibilities.items() for i in range(value)]
                 next_note = random.choice(notes_as_weighted_possibilities_list)
-            melody.append(next_note)
-        return "".join(melody)
+            given_start = melody[-1] + next_note
+            melody = melody + next_note            
+        return melody
+    
--- src/tests/markov_test.py
+++ src/tests/markov_test.py
@@ -3,8 +3,18 @@ from logic import markov_logic
 
 class TestTrie(unittest.TestCase):
     def setUp(self):
-        self.mc = markov_logic.MarkovChain("src/tests/test-notes/shortkey/", "A")
+        self.mc = markov_logic.MarkovChain("src/tests/test-notes/shortkey/", "D")
+        self.teaching_melody="azbabzgbcczddddecefzafgbagz"
 
-    def test_melody_generated_without_data_is_empty(self):
-        melody = self.mc.generate_melody()
-        self.assertTrue(melody == '')
+    def test_melody_beginswith_given_start(self):
+        melody = self.mc.generate_melody(given_start="dd")
+        self.assertEqual(melody[:2], 'dd')
+
+    def test_melody_end_contains_only_sequences_found_in_teaching_melody(self):
+        melody = self.mc.generate_melody(given_start="dd")
+        sequence_found = False
+        for i in range(2, len(melody)):
+            sequence_found = melody[i-2:i] in self.teaching_melody
+            if not melody[i-2:i]:
+                sequence_found=False
+        self.assertTrue(sequence_found)
